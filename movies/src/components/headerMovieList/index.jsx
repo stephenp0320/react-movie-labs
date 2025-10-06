296d7b1669ea58186859f6ff6eb007652056349c
stephenp0320 <stephen power>
2025-10-06 16:55:47 +0100

    Fix backward/forward arrows.

--- movies/src/components/headerMovieList/index.jsx
+++ movies/src/components/headerMovieList/index.jsx
@@ -5,32 +5,30 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 
-const Header = (props ) => {
+const Header = (props) => {
   const title = props.title
   return (
-    <Paper 
-      component="div" 
+    <Paper
+      component="div"
       sx={{
         display: "flex",
         justifyContent: "space-around",
         flexWrap: "wrap",
         marginBottom: 1.5,
       }}
-      >
-      <IconButton
-        aria-label="go back"
-      >
+    >
+      <IconButton aria-label="go back" onClick={() => navigate(-1)}>
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
+
 
       <Typography variant="h4" component="h3">
         {title}
       </Typography>
-      <IconButton
-        aria-label="go forward"
-      >
+      <IconButton aria-label="go forward" onClick={() => navigate(+1)}>
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
+
     </Paper>
   );
 };
