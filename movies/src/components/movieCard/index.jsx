eddbec1dfbdf2a4ffee1d46dbd481fc14d2104a8
stephenp0320 <stephen power>
2025-10-13 12:23:57 +0100

    Card icons - make action icons on movie card configurable.

--- movies/src/components/movieCard/index.jsx
+++ movies/src/components/movieCard/index.jsx
@@ -17,7 +17,7 @@ import { Link } from "react-router";
 import Avatar from '@mui/material/Avatar';
 
 
-export default function MovieCard({ movie }) { 
+export default function MovieCard({ movie, action }) {
   const { favorites, addToFavorites } = useContext(MoviesContext);
 
   if (favorites.find((id) => id === movie.id)) {
@@ -75,20 +75,17 @@ export default function MovieCard({ movie }) {
         </Grid>
       </CardContent>
       <CardActions disableSpacing>
-      <IconButton aria-label="add to favorites" onClick={handleAddToFavorite}>
-        <FavoriteIcon color="primary" fontSize="large" />
-    </IconButton>
+      
+      {action(movie)}
+    
+      <Link to={`/movies/${movie.id}`}>
+        <Button variant="outlined" size="medium" color="primary">
+          More Info ...
+        </Button>
+      </Link>
+      
+    </CardActions>
 
-
-        <Link to={`/movies/${movie.id}`}>
-          <Button variant="outlined" size="medium" color="primary">
-            More Info ...
-          </Button>
-        </Link>
-
-
-
-      </CardActions>
     </Card>
   );
 }
--- movies/src/pages/homePage.jsx
+++ movies/src/pages/homePage.jsx
@@ -3,6 +3,8 @@ import { getMovies } from "../api/tmdb-api";
 import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from '@tanstack/react-query';
 import Spinner from '../components/spinner';
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+
 
 const HomePage = (props) => {
 
@@ -28,10 +30,13 @@ const HomePage = (props) => {
 
   return (
     <PageTemplate
-      title='Discover Movies'
+      title="Discover Movies"
       movies={movies}
-      selectFavorite={addToFavorites}
+      action={(movie) => {
+        return <AddToFavoritesIcon movie={movie} />
+      }}
     />
-  );
+);
+
 };
 export default HomePage;
