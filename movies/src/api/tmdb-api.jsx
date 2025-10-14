fe37cba5c149b27c5cbf991d3e87c8a70dc2fcee
stephenp0320 <stephen power>
2025-10-14 11:19:08 +0100

    Added Upcoming movies page.

--- movies/src/api/tmdb-api.jsx
+++ movies/src/api/tmdb-api.jsx
@@ -93,3 +93,19 @@ export const getMovieImages = ({ queryKey }) => {
    });
   };
 
+
+  export const getUpcomingMovies = (page = 1) => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&page=${page}`
+    ).then((responce) => {
+      if (!responce.ok) {
+        return responce.json().then((error) => {
+          throw new Error(error.status_message);
+        });
+      }
+      return responce.json();
+    }).catch((error) => {
+      throw error;
+    });
+
+  };
