68025ac14870670d5acce4cb19f2917ba844849e
stephenp0320 <stephen power>
2025-10-14 11:59:43 +0100

    Tagging upcoming movies as must-watch done

--- movies/src/components/cardIcons/addToPlaylist.jsx
+++ movies/src/components/cardIcons/addToPlaylist.jsx
@@ -1,10 +1,18 @@
 import React, { useContext } from "react";
 import IconButton from "@mui/material/IconButton";
 import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+import { MoviesContext } from "../../contexts/moviesContext";
 
 const AddToPlaylistIcon = ({ movie }) => {
+  const {addToMustWatch} = useContext(MoviesContext)
+
+  const handleAddToMustWatch = (e) => {
+    e.preventDefault();
+    addToMustWatch(movie);
+  };
+
   return (
-    <IconButton aria-label="add to playlist">
+    <IconButton aria-label="add to playlist" onClick={handleAddToMustWatch}>
       <PlaylistAddIcon color="primary" fontSize="large" />
     </IconButton>
   );
--- movies/src/contexts/moviesContext.jsx
+++ movies/src/contexts/moviesContext.jsx
@@ -5,6 +5,7 @@ export const MoviesContext = React.createContext(null);
 const MoviesContextProvider = (props) => {
   const [favorites, setFavorites] = useState( [] )
   const [myReviews, setMyReviews] = useState( {} ) 
+  const [mustWatch, setMustWatch] = useState([]);
 
 
   const addToFavorites = (movie) => {
@@ -21,14 +22,22 @@ const MoviesContextProvider = (props) => {
   const addReview = (movie, review) => {
     setMyReviews( {...myReviews, [movie.id]: review } )
   };
-  //console.log(myReviews);
-
   
-  // We will use this function in the next step
+
   const removeFromFavorites = (movie) => {
     setFavorites( favorites.filter(
       (mId) => mId !== movie.id
     ) )
+  };
+
+  const addToMustWatch = (movie) => {
+    if(!mustWatch.includes(movie.id)) {
+      const updated = [...mustWatch, movie.id];
+      setMustWatch(updated)
+      console.log("list of must watch, ", updated)
+    } else {
+      console.log("already in must watch list, ", movie.id);
+    }
   };
 
   return (
@@ -38,6 +47,8 @@ const MoviesContextProvider = (props) => {
         addToFavorites,
         removeFromFavorites,
         addReview,
+        mustWatch,
+        addToMustWatch,
       }}
     >
       {props.children}
