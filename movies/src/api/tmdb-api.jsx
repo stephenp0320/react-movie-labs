93691604447b3fbe9fcec7657961cac0f64693b2
stephenp0320 <stephen power>
2025-10-26 08:29:02 +0000

    getRecommendations helper added

--- movies/src/api/tmdb-api.jsx
+++ movies/src/api/tmdb-api.jsx
@@ -217,3 +217,25 @@ export const getMovieImages = ({ queryKey }) => {
     });
 
   };
+
+
+
+  //https://api.themoviedb.org/3/movie/{movie_id}/recommendations
+
+
+  export const getRecommendations = () => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    ).then((response) => {
+      if (!response.ok) {
+        return response.json().then((error) => {
+          throw new Error(error.status_message || "Something went wrong");
+        });
+      }
+      return response.json();
+    })
+    .catch((error) => {
+        throw error
+    });
+  };
+ 
